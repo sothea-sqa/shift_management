bc2443739f3eacc7589160b21a138206c1a467a0
khannchy-dg <choun.khannchy@squeeze-inc.asia>
2025-01-16 11:58:11 +0700

    Create Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LogIn from './Authentication/LogIn';
+import Navbar from './Home/components/Navbar';
+
 
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+      
+        <Route path='/login' element={<LogIn/>} />
+        <Route path='/nav' element={<Navbar/>} />
 
       </Routes>
     </BrowserRouter>
